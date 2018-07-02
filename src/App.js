a3fea56422e950e75aad1cc6f6db613616624646
Jay <jaygpt@iitk.ac.in>
2018-07-02 14:09:54 +0000

    added basic info

--- src/App.js
+++ src/App.js
@@ -5,16 +5,23 @@ import web3 from './web3';
 import lottery from './lottery';
 
 class App extends Component {
-  constructor(props){
+  /* constructor(props){
     super(props);
     this.state = { manager: ''};
     
-  }
+  } */
+  state = {
+    manager : '',
+    Players : [],
+    balance: ''
+  };
   async componentDidMount() {
     const manager = await lottery.methods.manager().call();
+    const Players = await lottery.methods.seeAllplayer().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
     // we don't needs to write from since mattamask is intelligent enough
 
-    this.setState({manager: manager});
+    this.setState({manager,Players,balance});
   }
   
   render() {
@@ -28,6 +35,8 @@ class App extends Component {
         </header>
         <div>
         <p>This contract is managed by Jay Gupta(A/C: {this.state.manager})</p>
+        <p>There are total {this.state.Players.length} Players</p>
+        <p> With Total Balance In it {web3.utils.fromWei(this.state.balance, 'ether')} ether</p>
       </div>
       </div>
       
