4f4b1290ef2c6a5712ac8d70e3b09aa48c6c75e4
Jay <jaygpt@iitk.ac.in>
2018-07-02 13:36:29 +0000

    Initizalized App

--- src/App.js
+++ src/App.js
@@ -5,18 +5,32 @@ import web3 from './web3';
 import lottery from './lottery';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { manager: ''};
+    
+  }
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+    // we don't needs to write from since mattamask is intelligent enough
+
+    this.setState({manager: manager});
+  }
+  
   render() {
+
     web3.eth.getAccounts().then(console.log);
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Lottery App to try your Luck</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+        <p>This contract is managed by Jay Gupta(A/C: {this.state.manager})</p>
       </div>
+      </div>
+      
     );
   }
 }
